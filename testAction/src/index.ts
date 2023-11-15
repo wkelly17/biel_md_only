fdae18a322d3bdfe907b593a8c9772ba60e72c8a
Will Kelly <willkelly9617@gmail.com>
2023-11-15 11:26:54 -0600

    edit gh action 8

--- testAction/src/index.ts
+++ testAction/src/index.ts
@@ -2,7 +2,7 @@ import fs, {writeFileSync} from "node:fs";
 
 let rootDir = process.cwd();
 console.log({rootDir});
-let text_file_location = rootDir + "README.md";
+let text_file_location = rootDir + "/README.md";
 let text_file_location2 = rootDir + "/src/content/pages/one.mdx";
 let data = fs.readFileSync(text_file_location, {
   encoding: "utf-8",
